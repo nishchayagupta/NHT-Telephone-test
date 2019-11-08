cf2538ae17e6b47280b351315a33c634e47c3bab
nishchayagupta <nishchayagupta@gmail.com>
2019-11-08 10:53:46 -0500

    component did mount method added

--- components/homeScreen.js
+++ components/homeScreen.js
@@ -26,19 +26,58 @@ export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputText: ""
+      inputText: "",
+      left: [],
+      right: []
     };
     this.returnButton = this.returnButton.bind(this);
   }
 
+  // This method will select 20 files randomly for each ear and add it to the state variables left and right
+  componentDidMount() {
+    Leftvisited = [];
+    //randomly selecting 30 files for the left ear
+    while (Leftvisited.length < 30) {
+      randNumber = Math.floor(Math.random() * 64);
+      if (Leftvisited.includes(audioFiles[randNumber])) {
+        continue;
+      } else {
+        Leftvisited.push(audioFiles[randNumber]);
+      }
+    }
+    //setting state variable left according to the audio files selected
+    this.setState({ left: Leftvisited });
+
+    RightVisited = [];
+    //randomly selecting 30 files for the right ear which haven't been used for the left ear
+    while (RightVisited.length < 30) {
+      randNumber = Math.floor(Math.random() * 64);
+      if (
+        Leftvisited.includes(audioFiles[randNumber]) ||
+        RightVisited.includes(audioFiles[randNumber])
+      ) {
+        continue;
+      } else {
+        RightVisited.push(audioFiles[randNumber]);
+      }
+    }
+    // setting state variable right according to the 30 files selected
+    this.setState({ right: RightVisited });
+  }
+
+  // This method would append the text value entered by the user into the top text show area
   appendState = num => {
     this.setState({ inputText: this.state.inputText + num });
   };
 
+  //This method would remove the last character from the string entered in the top text area
   backspace = () => {
     this.setState({ inputText: this.state.inputText.slice(0, -1) });
   };
 
+  // This function would return blank views in order to maintain spacing between components in the final render function.
+  // The function intakes the num argument as the num of space views and val argument in order to assign a unique key to
+  // each view which is an important aspect for react components
   spaceView = (num, val) => {
     returnDiv = [];
     for (let i = 0; i < num; i++) {
@@ -52,6 +91,8 @@ export default class HomeScreen extends React.Component {
     );
   };
 
+  // This function returns a button component according to the num argument passed to it. This button would be titled
+  // same as the argument passed and would also work as a button (i.e. append the value clicked to the top text section)
   returnButton = num => {
     return (
       <Button
