2eb623ab4c7d004a0b694417f3bb18442bea4911
nishchayagupta <nishchayagupta@gmail.com>
2020-03-08 19:56:36 -0400

    audio unloading implemented

--- components/leftPlayer.js
+++ components/leftPlayer.js
@@ -1,28 +1,34 @@
 import { Audio } from "expo-av";
 import * as AudioFiles from "./audioSelector";
 
+// async function playSound(sound) {
+//   setTimeout(() => {
+//     Audio.Sound
+//       .createAsync(sound, { shouldPlay: true })
+//       .then(res => {
+//         res.sound.setOnPlaybackStatusUpdate(status => {
+//           if (!status.didJustFinish) return;
+//           console.log("Unloading ");
+//           res.sound.unloadAsync().catch(() => {});
+//         });
+//       })
+//       .catch(error => {});
+//   }, 1000);
+// }
+
 // Play audio file
 export const playLeftSound = async audioTrack => {
-  const soundObject = new Audio.Sound();
-  try {
-    await soundObject.loadAsync(AudioFiles.audioSelector(audioTrack));
-    setTimeout(() => {
-      soundObject.playAsync();
-      console.log("play the sound");
-    }, 2000);
-
-    //   this.setState({ soundFlag: 1 });
-    //   if (this.state.currentEar == "left") {
-    //     var leftCount = this.state.leftCounter + 1;
-    //     this.setState({ leftCounter: leftCount });
-    //   } else if (this.state.currentEar == "right") {
-    //     var rightCount = this.state.rightCounter + 1;
-    //     console.log("new right count is " + rightCount);
-    //     this.setState({ rightCounter: rightCount });
-    //   }
-
-    // Your sound is playing!
-  } catch (error) {
-    console.log("error playing sound due to ", error);
-  }
+  sound = AudioFiles.audioSelector(audioTrack);
+  setTimeout(() => {
+    Audio.Sound
+      .createAsync(sound, { shouldPlay: true })
+      .then(res => {
+        res.sound.setOnPlaybackStatusUpdate(status => {
+          if (!status.didJustFinish) return;
+          console.log("Unloading ");
+          res.sound.unloadAsync().catch(() => {});
+        });
+      })
+      .catch(error => {});
+  }, 1000);
 };
